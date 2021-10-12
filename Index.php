4a5517003bbb9352788c548e07344f894e66a24d
BadRat-in <67710516+BadRat-in@users.noreply.github.com>
2021-10-12 14:57:10 +0530

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 if (!isset($_SESSION["auth"])) {
         header("location: ./home.php");
@@ -5,4 +6,13 @@ if (!isset($_SESSION["auth"])) {
 else{
     header("location: ./main.php");
 }
+=======
+<?php
+if (!isset($_SESSION["auth"])) {
+        header("location: ./home.php");
+    }
+else{
+    header("location: ./main.php");
+}
+>>>>>>> 5eec0f63818cddd420aba2420a65c58249618f7c
 ?>
